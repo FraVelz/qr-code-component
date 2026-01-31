97b3eff2102ae17081ced438c0674113ea68fbb0
Fravelz <fravelz_@hotmail.com>
2026-01-31 15:53:21 -0500

    Actualización: 0.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output:'static',
+  base:'/qr-code-component/',
+  site:'https://fravelz.github.io/qr-code-component/',
+
   vite: {
     plugins: [tailwindcss()]
   }
